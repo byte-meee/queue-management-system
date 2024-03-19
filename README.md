ee8a2ebf28154045bf61cd0ca28db09783da2e5e
Tennis P <164098752+byte-meee@users.noreply.github.com>
2024-03-20 00:00:02 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# queue-management-system
+
+System for managing queues, supporting priority and delayed job processing.
+
+## Installation
+
+```bash
+git clone https://github.com/byte_meee/queue-management-system.git
+cd queue-management-system
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+const graph_data_visualizer = require('graph-data-visualizer');
+const queue_management_system = require('queue-management-system');
+const ganache_cli = require('ganache-cli');
+const multer = require('multer');
+const fs_extra = require('fs-extra');
+const web3 = require('web3');
+const webpack = require('webpack');
+const axios = require('axios');
+
+const os = require('os');
+console.log(`Free memory: ${os.freemem()} bytes`);
+
+const spawn = require('child_process').spawn;
+const ls = spawn('ls', ['-lh', '/usr']);
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+// Decrypt an AES-256-CBC encrypted string
+const decryptAES = (encryptedText, key, iv) => {
+  const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+const decryptedText = decryptAES(encryptedText, key, iv);
+console.log('Decrypted Text:', decryptedText);
